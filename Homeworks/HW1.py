5c788f435182862b4f4c7911b44e53afe056243d
mtaelp <metealpbahadir@gmail.com>
2021-02-21 22:50:57 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,27 @@
-#Explain your work
+import random
 
-#Question 1
-for x in range(a):
-	print(a)
+a = [[], [], []]
+while 1:
+    b = random.randint(0, 99999)
+    if b > 1:
+        for i in range(2, b):
+            if (b % i) == 0:
+                print(b, "is not a prime number")
+                break
+        else:
+            print(b, "is a prime number")
+            if len(a[0]) < 3:
+                a[0].append(b)
+            elif len(a[1]) < 3:
+                a[1].append(b)
+            elif len(a[2]) < 3:
+                a[2].append(b)
+            else:
+                break
+
+
+
+    else:
+        print(b, "is not a prime number")
+for i in a:
+    print(i)
