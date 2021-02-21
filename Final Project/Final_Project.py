40ebee76d983f5ae8d46002bdc9d81497aa3dbde
mtaelp <metealpbahadir@gmail.com>
2021-02-22 02:58:53 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,131 @@
-import pandas as pd
 
-data = pd.read_csv("project.csv")
+class tatlilar:
+
+    def kakao(self):
+        genel.gerekli(self)
+        malzemeler= ["Un", "Kabartma Tozu", "Yumurta", "Toz Şeker", "Vanilya", "Kakao", "Sıvı Yağ", "Sütlü Çikolata","Tereyağı"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nADİM 1- Un hariç bütün malzemeleri yoğurma kabında karıştıralım. Yavaş yavaş unu ekleyip (aldığı kadar eklemeye dikkat edin. Bardağına göre ölçüsü fazla ya da az gelebilir) yumuşak olmayacak şekilde bir hamur elde edelim.\nADİM 2- Bekletmeden ceviz büyüklüğünde elimizde yuvarlayıp yağladığımız tepsiye dizelim.\nADİM 3- Önceden ısıtılmış 170 derecelik fırında 25 dakika civarında pişirelim.\nADİM 4- Kurabiyelerimiz pişerken soğuk süt ve  3 yemek kaşığı daha şekeri bir kaba koyarak şekeri sütun içinde eriyene kadar karıştıralım.\nADİM 5- Fırından çıkan sıcak kurabiyelerimizi soğuk süte atıp iyice ıslandıktan sonra servis tabağına dizelim. Çikolatayı benmarı usulü eritip kurabiyenin üzerinde gezdirip servis edelim.")
+    def Çikolatalı(self):
+        genel.gerekli(self)
+        malzemeler = ["Yumurta", "Ülker Çokokrem"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nADİM 1- Yumurtanın sadece aklarını mikserle kopuk kıvamına gelene kadar çırpıyoruz. (Sarılarını yüzünüze maske yapmak için kullanabilirsiniz)\nADİM 2- Çikolatayı karışıma ekleyip söndürmeden yavaşça karıştırıyoruz ve sufle kalıplarına alıyoruz. Dilediğiniz marka sürülebilir çikolata kullanabiliriniz.\nADİM 3- Kapılarımızı derin bir tencereye koyup yarısına kadar sıcak şu ekliyoruz, tencerenin kapağını kapatıp kısık ateşte kek 10 dakika pişiriyoruz. İsterseniz üzerine kakao serpebilirsiniz.")
+    def Sufle(self):
+        genel.gerekli(self)
+        malzemeler = ["Un", "Yumurta", "Toz Şeker", "Vanilya", "Bitter Çikolata", "Tereyağı"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nADIM 1-Tereyağ ve çikolatayı benmari usulü eritiyoruz.\nADIM 2- 3 adet yumurta ve 1 adet yumurta sarısını bir kaba alıp 2 dk kadar çırpıyoruz. Daha sonra üzerine un şeker ve vanilyayı ekliyoruz ve iyice karıştırıyoruz. Hiçbir pütür kalmayıncaya kadar karıştırmayı sürdürüyoruz.\nADIM 3- Daha sonra benmari usulü erittiğimiz çikolata ve tereyağını ekliyoruz ve iyice karıştırıyoruz. İşte sufle harcımız hazır.\nADIM 4-Ardından, yapacağımız sufle kaplarını tereyağ ile yağlayıp unluyoruz. Ve yaptığımız karışımı içine döküyoruz. Buzdolabında 15 dk dinlendiriyoruz ve daha sonra önceden ısıtılmış alt üst ayar fırında 200°C de 10 dk pişiriyoruz.")
+
+class atis:
+    def mantar(self):
+        genel.gerekli(self)
+        malzemeler= ["Karabiber", "Süt", "Tuz", "Rendelenmiş Kaşar Peyniri", "İnce Kıyılmış Maydanoz", "Bayat Ekmek Dilimi", "Pul Biber", "Tereyağı","Büyük Boy Mantar"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nAdım 1- Mantarların saplarını çıkarıp temizleyin. Fırın tepsisine dizin ve eritilmiş tereyağını içlerine pay edin.\nAdım 2- Önceden ısıtılmış 180 derece fırında 10 dakika pişirin. Bir kasede, ufalanmış bayat ekmek, kaşar peyniri, süt, maydanoz, tuz, karabiber ve pul biberi karıştırın.\nAdım 3- Bu karışımdan parçalar alıp avucunuzun içinde sıkıştırın ve pişen mantarların ortasına yerleştirin. Yeniden fırına verip 10 dakika daha pişirin ve sıcak servis yapın.")
+    def Makarna(self):
+        genel.gerekli(self)
+        malzemeler = ["Karabiber", "Yoğurt", "Tuz", "Galeta Unu", "Sıvı Yağ", "Su", "Pul Biber", "Nane","Mayonez","Kelebek Makarna"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("n\ADIM 1- Makarnayı haşlayıp suzun. Kuruması için düz bir tepsiye alın.\nADİM 2- Üzerine sıvı yağ, galeta ünü, tuz, karabiber ve toz kırmızibiber ekleyip harmanlayın.\nADİM 3- Bol yağda kızartin. Fazla yağını almak için kağıt havlu üzerine çıkarın. Dip sos için yoğurt ve mayonezi birlikte çırpın.\nADİM 4-Üzerine tuz, karabiber, pul biber ve nane ekleyip karıştırın. Makarna cipsleri ile birlikte servis yapın.")
+
+class ana:
+    def somon(self):
+        genel.gerekli(self)
+        malzemeler= ["Krema", "Parmesan ", "Rokfor ", "Somon Füme"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nADIM 1- Rokfor ve Parmesan peyniri rendelenir.\nADIM 2- Rendelenen peynirler küçük bir tavaya alınır ve krema eklenir.\nADIM 3- Peynirler eriyince kadar kısık ateşte bırakılır. Eriyen peynirler içine tercihe göre maydonoz, dereotu, nane, kereviz tohumu eklenir.\nADIM 4- Küçük kâseye koyup dondurucuya konur. Donan karışımımız dondurucudan çıkarıp sarma sarar misali somon ile buluşur. Üzerine hafif zeytinyağı ile servis edilir.")
+
+    def piras(self):
+        genel.gerekli(self)
+        malzemeler = ["Pırasa", "Zeytinyağı", "Somon", "Tuz ve Karabiber"]
+        for i in range(len(malzemeler)): print(malzemeler[i])
+        print("\nADİM 1-Somonu temizleyip ve iri iri küpler halinde keselim.\nADİM 2-Pirasanın yeşil kısmını kesip yıkayalım ve 2’şer parmak kalınlığında keselim.\nADİM 3- İnce çöplerimize bir somon küpü, bir pirasa dilimi dizelim. Tuz serpip fırça ile yağlayalım.\nADİM 4- Sıcak izgarada somonlar ve pirasalar kızarıncaya kadar kızartalım ve hemen servis edelim. Afiyet olsun…")
+
+
+
+
+
+class genel:
+     def gerekli(self):
+        print("Gerekli Malzemeler\n")
+
+
+class tatli:
+    def decided(self):
+         while True:
+            listofthings = ["1. Çikolatalı Akiskan Sufle","2. Çikolatalı Pratik Sufle","3. Kakao Şöleni Kurabiye"]
+            for i in range(0,len(listofthings)): print(" "+listofthings[i])
+            a = input("hangisini istersiniz\n→")
+            if a == "1":
+                tatlilar.Çikolatalı(self)
+                break
+            elif a == "2":
+                tatlilar.Sufle(self)
+            elif a == "3":
+                tatlilar.kakao(self)
+
+                break
+            else:
+                print("hatali giris yaptiniz")
+class s_atis:
+    def decided(self):
+         while True:
+            listofthings = ["1. Mantar Çanakları","2. Makarna Cipsi"]
+            for i in range(0,len(listofthings)): print(" "+listofthings[i])
+            a = input("hangisini istersiniz\n→")
+            if a == "1":
+                atis.mantar(self)
+                break
+            elif a == "2":
+                atis.Makarna(self)
+
+                break
+            else:
+                print("hatali giris yaptiniz")
+
+
+
+class sana:
+    def decided(self):
+         while True:
+            listofthings = ["1. Pırasalı Somon Şiş","2. Somon Sarma"]
+            for i in range(0,len(listofthings)): print(" "+listofthings[i])
+            a = input("hangisini istersiniz\n→")
+            if a == "1":
+                ana.piras(self)
+                break
+            elif a == "2":
+                ana.somon(self)
+
+                break
+            else:
+                print("hatali giris yaptiniz")
+
+class anamenu(tatli,s_atis,sana):
+    def __init__(self):
+        print("tarif uygulamasina hosgeldiniz")
+        types = ["1. tatli ","2. ana yemek ","3. atistirmaliklar ",]
+        for i in range(0,len(types)):
+            print(types[i])
+        while True:
+            opt = input("→ lutfen tarif turunu secin \n")
+            if opt == "1":
+                tatli.decided(self)
+                break
+            elif opt == "2":
+                sana.decided(self)
+                break
+            elif opt == "3":
+                s_atis.decided(self)
+                break
+            else:
+                print("yanlis numara yazdin")
+
+while True:
+    anamenu()
+    a = input("cikamak icin q ya | ana menuye donmek icin herhangi bir tusa basin \n»")
+    if a == "q" or a == "Q":
+        break
+    else:
+        anamenu()
